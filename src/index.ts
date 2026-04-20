91bf71992be9e6c596d9c99ed82dbfab5b9a2bae
Love Holmgren <143357495+hevolx@users.noreply.github.com>
2026-04-20 16:25:18 +0200

    feat(car): add Car class implementing AutonomousCar interface

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
 import { getObstacleEvents } from './computer-vision';
+
+interface AutonomousCar {
+  isRunning?: boolean;
+}
+
+interface AutonomousCarProps {
+  isRunning?: boolean;
+}
+
+class Car implements AutonomousCar {
+  isRunning?: boolean;
+
+  constructor(props: AutonomousCarProps) {
+    if (props.isRunning !== undefined) {
+      this.isRunning = props.isRunning;
+    }
+  }
+}
+
+const autonomousCar = new Car({ isRunning: true });
+console.log(autonomousCar.isRunning);
