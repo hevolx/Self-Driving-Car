3868364c1d8cc2aa20e6c431ac169662b4e761ee
Love Holmgren <143357495+hevolx@users.noreply.github.com>
2026-04-20 17:18:38 +0200

    feat(car): implement obstacle avoidance logic in respond method
      Iterate over all obstacle events and trigger steering turns based on
      event type. ObstacleLeft triggers a right turn, all other events
      trigger a left turn. Skips events where the value is false.
    
      Also adds a TODO comment with extra credit suggestions for future
      development.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 import { getObstacleEvents } from './computer-vision';
 
+/**
+ * TODO (extra credit):
+ * 1. Write code that will call .respond() many times with new events to see a sequence of turns.
+ * 2. Write code that allows the car to accelerate or decelerate based on the event.
+      You could make this another type that extends the Control type.
+ * 3. Add more types of events so your car can do things like ‘emergencyBrake’ or ‘parallelPark’.
+ */
+
 interface AutonomousCar {
   isRunning?: boolean;
   respond(events: Events): void;
@@ -44,7 +52,16 @@ class Car implements AutonomousCar {
 
   respond(events: Events) {
     if (this.isRunning == true) {
-      return events;
+      let event = Object.keys(events);
+      event.forEach((eventKey) => {
+        if (events[eventKey] == false) {
+          return
+        } else {
+          if (this.steeringControl !== undefined) {
+            this.steeringControl.turn(eventKey == 'ObstacleLeft' ? 'right' : 'left')
+          }
+        }
+      })
     } else {
       return console.log('The car is off');
     }
@@ -54,4 +71,3 @@ class Car implements AutonomousCar {
 const steering = new SteeringControl()
 const autonomousCar = new Car({ isRunning: true, steeringControl: steering });
 autonomousCar.respond(getObstacleEvents());
-console.log(autonomousCar);
